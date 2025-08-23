239dc0d444fd418c89fdfac5ed66a96e1dd244c1
Dzzhodddi <d.dzhos@ukma.edu.ua>
2025-08-23 21:14:31 +0300

    add vars file

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "postgres_user" {
+  type    = string
+}
+
+variable "postgres_password" {
+  type    = string
+}
+
+variable "postgres_db" {
+  type    = string
+}
+
+variable "postgres_host" {
+  type    = string
+}
+
+variable "postgres_port" {
+  type = number
+}
+
+variable "email" {
+  type = string
+}
